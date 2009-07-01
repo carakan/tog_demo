382df387053f3f7efccdf4e1d1a1b3c40f16d729
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2009-07-01 21:59:39 +0200

    removed v0.4 version

--- config/deploy/staging.rb
+++
@@ -1,6 +0,0 @@
-set :app_domain, "tog.larnaby.net"
-set :mongrel_conf, "#{deploy_to}/current/config/mongrel/staging.yml"
-
-role :app, "tog.larnaby.net"
-role :web, "tog.larnaby.net"
-role :db,  "tog.larnaby.net", :primary => true
--- vendor/plugins/acts_as_abusable/lib/abuse.rb
+++
@@ -1,9 +0,0 @@
-class Abuse < ActiveRecord::Base
-  belongs_to :resource, :polymorphic => true
-  
-  def confirm!
-    self.confirmed = true
-    self.save unless self.new_record?
-  end
-  
-end
